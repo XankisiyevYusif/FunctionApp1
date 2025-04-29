6a2658bcfbd7d1fdee34a48953821998f4208586
Xanki_be14 <Xanki_be14@STHQ0118-14>
2025-04-29 11:34:59 +0400

    error

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Net.Mail;
+using System.Web;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -19,10 +21,38 @@ namespace FunctionApp1
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            var queryParams = HttpUtility.ParseQueryString(req.Url.Query);
+            var gmail = queryParams["gmail"];
+
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
+            string fromAddress = "yusif2006ofi@gmail.com";
+            string toAddress = $"{gmail}";
+            string subject = "Gmail from Azure Function";
+            string body = "Hello, this is a Azure Function email";
+
+            MailMessage mail = new MailMessage(fromAddress, toAddress, subject, body);
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587, 
+                Credentials = new NetworkCredential("yusif2006ofi@gmail.com", "@Yusif200628711com@"), 
+                EnableSsl = true 
+            };
+
+            try
+            {
+                smtpClient.Send(mail);
+                response.WriteString("Email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                response.WriteString("Error sending email: " + ex.Message);
+            }
+
             response.WriteString("Welcome to Azure Functions!");
+            response.WriteString($"{gmail}");
 
             return response;
         }
